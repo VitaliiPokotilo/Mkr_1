7d7d55d94066f3c2a05157011b0afddce03b08c5
VitaliiPokotilo <vitaliipokotilo15@gmail.com>
2025-03-28 11:05:25 +0200

    Implement file comparison logic

--- file_comparator.py
+++ file_comparator.py
@@ -4,6 +4,18 @@ def read_file_lines(filename):
         return set(file.read().splitlines())
 
 
+def compare_files(file1, file2):
+    """
+    Compare two text files and return:
+    - lines present in both files
+    - lines present only in one of the files
+    """
+    lines1 = read_file_lines(file1)
+    lines2 = read_file_lines(file2)
 
+    same_lines = lines1 & lines2
+    diff_lines = lines1.symmetric_difference(lines2)
+
+    return same_lines, diff_lines
 
 
